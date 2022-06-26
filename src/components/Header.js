90ce982057e8411f075bb3c25f2bc1307c105eb1
soni <sonygea@gmail.com>
2022-06-26 12:56:06 +0700

    Feat : Membuat situs sederhana, melengkapi komponen di contact.js dan header.js serta membuat file contact.css dan header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div className="header">
+      <h1>Call a Friend</h1>
+      <h5>Your friendly contact app</h5>
+      <div className="dot">
+        <span>.....................</span>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
